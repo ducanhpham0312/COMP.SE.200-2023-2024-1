1fc5c8a0ec1fe3d02954818144e6604cb95c1f0d
ducanhpham <ducanh.pham@elisa.fi>
2023-11-18 23:50:28 +0200

    feat: add first test

---
+++ test/isDate.test.js
@@ -0,0 +1,8 @@
+const isDate = require('../src/isDate');
+
+describe('isDate', function(){
+    it('should return true with Date object', function(){
+        const date = new Date();
+        expect(2).toBe(2)
+    });
+});
