0df5722c068d47b190d06d39caf0f9096d9d01de
RECORDON ELISA <elisa.recordon@insa-rouen.fr>
2023-12-03 17:26:04 +0200

    test: get - new TC: no default value + other paths to explore

--- test/get.test.js
+++ test/get.test.js
@@ -1,17 +1,43 @@
 import get from "../src/get.js";
 
 describe('get', function() {
-    const object = {'a': [{'b': {'c': 3}}]}
-    it('should return 3 when given the right path syntax', function() {
+    const object = {'a': [{'b': {'c': 3}},
+			  {'d': {'e': 5}}
+			 ]};
+
+    it('return 3 when given the right path syntax (string format)', function() {
 	expect(get(object, 'a[0].b.c', 'default')).toBe(3);
     });
-    it("should return the default value when the path syntax's wrong", function() {
+
+    it('return 3 when given another correct path syntax (list format)', function() {
+	expect(get(object, ['a', '0', 'b', 'c'], 'default')).toBe(3);
+    });
+
+    it('return 5 when given a correct path (string) but search in another branch of the object', function() {
+	expect(get(object, 'a[1].d.e')).toBe(5);
+    });
+
+    it('return 5 when given a correct path (list) but search in another branch of the object', function() {
+	expect(get(object, ['a', '1', 'd', 'e'])).toBe(5);
+    });
+    
+    it("return the default value when the path syntax's wrong", function() {
 	expect(get(object, 'a[0].c.b', 'default')).toBe('default');
     });
-    it('should return the default value when given an empty array', function() {
+    
+    it('return the default value when given an empty array', function() {
 	expect(get([], 'a.b.c', 'default')).toBe('default');
     });
-    it('should return the default value when given null', function() {
+
+    it('return undefined when given an empty array without default value', function() {
+	expect(get([], 'a.b.c')).toBe(undefined);
+    });
+    
+    it('return the default value when given null', function() {
 	expect(get(null, 'a.b.c', 'default')).toBe('default');
+    });
+
+    it('return undefined when given null without default value', function() {
+	expect(get(null, 'a.b.c')).toBe(undefined);
     });
 });
