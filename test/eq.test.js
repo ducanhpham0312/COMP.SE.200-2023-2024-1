2c2665fd1a5237fea4b74b9a42b782ef6c7b343c
RECORDON ELISA <elisa.recordon@insa-rouen.fr>
2023-12-04 00:53:53 +0200

    style: fix indentation

--- test/eq.test.js
+++ test/eq.test.js
@@ -8,79 +8,79 @@ describe('eq', function() {
     const array2 = ['a', '3', 'b']
     
     it('an object equals itself', function() {
-	expect(eq(object, object)).toBe(true);
+        expect(eq(object, object)).toBe(true);
     });
     
     it('2 equivalent objects but with different references are not equal', function() {
-	expect(eq(object, other)).toBe(false);
+        expect(eq(object, other)).toBe(false);
     });
 
     it('an array equals itself', function() {
-	expect(eq(array1, array1)).toBe(true);
+        expect(eq(array1, array1)).toBe(true);
     });
     
     it('2 different arrays are not equal', function() {
-	expect(eq(array1, array2)).toBe(false);
+        expect(eq(array1, array2)).toBe(false);
     });
     
     it('2 equivalent arrays but with different references are not equal', function() {
-	expect(eq(array1, array1_)).toBe(false);
+        expect(eq(array1, array1_)).toBe(false);
     });
   
     it('2 equivalent empty arrays (with different references) are not equal', function() {
-	expect(eq([], [])).toBe(false);
+        expect(eq([], [])).toBe(false);
     });
     
     it('2 identical strings are equal', function() {
-	expect(eq('a', 'a')).toBe(true);
+        expect(eq('a', 'a')).toBe(true);
     });
 
     it('2 different strings are not equal', function() {
-	expect(eq('a', 'b')).toBe(false);
+        expect(eq('a', 'b')).toBe(false);
     });
 
     it('2 identical integers are equal', function() {
-	expect(eq(3, 3)).toBe(true);
+        expect(eq(3, 3)).toBe(true);
     });
 
     it('2 different integers are not equal', function() {
-	expect(eq(3, 4)).toBe(false);
+        expect(eq(3, 4)).toBe(false);
     });
 
     it('negative zero equals positive zero', function() {
-	expect(eq(-0, 0)).toBe(true);
+        expect(eq(-0, 0)).toBe(true);
     });
 
     it('2 identical floats are equal', function() {
-	expect(eq(3.1, 3.1)).toBe(true);
+        expect(eq(3.1, 3.1)).toBe(true);
     });
 
     it('2 different floats are not equal', function() {
-	expect(eq(3.1, 2.4)).toBe(false);
+        expect(eq(3.1, 2.4)).toBe(false);
     });
 
     it('an integer equals the corresponding float', function() {
-	expect(eq(2, 2.0)).toBe(true);
+        expect(eq(2, 2.0)).toBe(true);
     });
 
     it('Infinity equals Infinity', function() {
-	expect(eq(Infinity, Infinity)).toBe(true);
+        expect(eq(Infinity, Infinity)).toBe(true);
     });
     
     it('NaN equals NaN', function() {
-	expect(eq(NaN, NaN)).toBe(true);
+        expect(eq(NaN, NaN)).toBe(true);
     });
 
     it('undefined equals undefined', function() {
-	expect(eq(undefined, undefined)).toBe(true);
+        expect(eq(undefined, undefined)).toBe(true);
     });
 
     it('null equals null', function() {
-	expect(eq(null, null)).toBe(true);
+        expect(eq(null, null)).toBe(true);
     });
 
     it('empty strings are equal', function() {
-	expect(eq("", "")).toBe(true);
+        expect(eq("", "")).toBe(true);
     });
 
     /** Failed tests
